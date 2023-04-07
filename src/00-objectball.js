29ba25b06818998638e4215bb93b2228d8afbd88
Juan Salazar <ferthelet@gmail.com>
2023-04-07 09:22:28 -0500

    defines teams' objects and sample function

---
+++ src/00-objectball.js
@@ -0,0 +1,132 @@
+function gameObject() {
+// contains and returns a nested object 
+    return {
+        home: {
+            // points to a string of the team name.
+            teamName: "Brooklyn Nets",
+            // points to an array of strings that are that team's colors.
+            colors: ["Black", "White"],
+            // points to an object of players whose names (as strings) are the keys to a object containing their stats
+            players: {
+                "Alan Anderson": {
+                    number: 0,
+                    size: 16,
+                    points: 22,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 1,
+                },
+                "Reggie Evans": {
+                    number: 30,
+                    size: 14,
+                    points: 12,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slamDunks: 7,
+                },
+                "Brook Lopez": {
+                    number: 11,
+                    size: 17,
+                    points: 27,
+                    rebounds: 19,
+                    assists: 10,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 15,
+                },
+                "Mason Plumlee": {
+                    number: 1,
+                    size: 19,
+                    points: 26,
+                    rebounds: 12,
+                    assists: 6,
+                    steals: 3,
+                    blocks: 8,
+                    slamDunks: 5,
+                },
+                "Jason Terry": {
+                    number: 31,
+                    size: 15,
+                    points: 19,
+                    rebounds: 2,
+                    assists: 2,
+                    steals: 4,
+                    blocks: 11,
+                    slamDunks: 1,
+                }
+            }
+        },
+        away: {
+            // points to a string of the team name.
+            teamName: "Charlotte Hornets",
+            // points to an array of strings that are that team's colors.
+            colors: ["Turquoise", "Purple"],
+            // points to an object of players whose names (as strings) are the keys to a object containing their stats
+            players: {
+                "Jeff Adrien": {
+                    number: 4,
+                    size: 18,
+                    points: 10,
+                    rebounds: 1,
+                    assists: 1,
+                    steals: 2,
+                    blocks: 7,
+                    slamDunks: 2,
+                },
+                "Bismak Biyombo": {
+                    number: 0,
+                    size: 16,
+                    points: 12,
+                    rebounds: 4,
+                    assists: 7,
+                    steals: 7,
+                    blocks: 15,
+                    slamDunks: 10,
+                },
+                "DeSagna Diop": {
+                    number: 2,
+                    size: 14,
+                    points: 24,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 4,
+                    blocks: 5,
+                    slamDunks: 5,
+                },
+                "Ben Gordon": {
+                    number: 8,
+                    size: 15,
+                    points: 33,
+                    rebounds: 3,
+                    assists: 2,
+                    steals: 1,
+                    blocks: 1,
+                    slamDunks: 0,
+                },
+                "Brendan Haywood": {
+                    number: 33,
+                    size: 15,
+                    points: 6,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 22,
+                    blocks: 5,
+                    slamDunks: 12,
+                }
+            }
+        }
+
+    }
+}
+console.log(gameObject());
+
+function homeTeamName () {
+    // returns the name of the home team
+    let object = gameObject();
+    return object["home"]["teamName"];
+}
+
